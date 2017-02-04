0cb310f1249b1dab7aef3f60ab80dbb609c8bf72
Dipayan553 <dipayanmondal553@gmail.com>
2017-02-04 14:34:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ element.innerHTML='New Value';
 
 //Move the image
 var img = document.getElementById('madi');
-img.onclick = function(){
+img.onclick = function () {
   img.style.marginleft = '100px';  
 };
