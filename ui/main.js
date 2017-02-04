62ae6cb1e93a8b25a87d5ca57bb75c2388d8c8d6
Dipayan553 <dipayanmondal553@gmail.com>
2017-02-04 14:45:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,18 @@
 console.log('Loaded!');
 
 //change the text of main-text div
-var element = document.getElementById('main-text'
-);
+var element = document.getElementById('main-text');
 
 element.innerHTML='New Value';
 
 //Move the image
+
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight(){
+    marginleft = marginleft+10;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function () {
-  img.style.marginleft = '100px';  
-};
+  var interval = setInterval(moveRight, 100);
+  };
