21732107547aa0f843fdd3c9c49f43fe3d55bfb4
Dipayan553 <dipayanmondal553@gmail.com>
2017-02-04 19:05:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,7 @@ button.onclick = function() {
       if(request.readyState === XMLHttpRequest.DONE){
           //Take some action
           if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
+            
           }
       }
       //Not done yet
@@ -29,14 +27,32 @@ button.onclick = function() {
  var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
  submit.onclick = function(){
-   //Make a request to the server andsend the name
-   
-   //Capture a list of names render it as a list
-   var names = ['name1','name2','name3','name4'];
+     //Create a request object
+     var request = new XMLHttpRequest;
+      //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status === 200){
+             //Capture a list of names render it as a list
+   var names = request.responseText;
+   names = JSON.parse(names);
    var list = '';
    for(var i=0; i<names.length; i++){
        list == '<li>' + names[i] + '</li>';
    }
    var ul = document.getElementById('namelist');
    ul.innerHTML = list;
+ }
+          }
+      };
+      //Not done yet
+  };
+ 
+//Make the request 
+ request.open('GET','http://dipayan553.imad.hasura-app.io/submit-name?name' + name,true);
+ request.send(null);
  };
+   //Make a request to the server andsend the name
+   
+  
